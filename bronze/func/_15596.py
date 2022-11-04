6226c5bb99c0ff2707dfed427e8270bd5e0c9bf1
hong <sayskpayss@naver.com>
2022-11-04 15:09:42 +0900

    22-11-04

---
+++ bronze/func/_15596.py
@@ -0,0 +1,8 @@
+#
+# def solve(a):
+#     total = 0
+#     for i in a:
+#         total += i
+#     return total
+
+solve = lambda a : sum(a)
--- bronze/two_demention_array/__init__.py
+++ bronze/two_demention_array/__init__.py
@@ -1,6 +1,13 @@
 import sys
 
-input = sys.stdin.readline()
+n,m = map(int, sys.stdin.readline().split())
 
+a = [list(map(int, sys.stdin.readline().split()) for _ in range(n))]
+b = [list(map(int, sys.stdin.readline().split()) for _ in range(m))]
 
-print(input)
+for i in range(n):
+    for j in range(m):
+        a[i][j] += b[i][j]
+
+for i in a:
+    print(*i)
