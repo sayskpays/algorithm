98c4dc6c584f0cae9406da91ffa603f4bbd8b425
hong <sayskpayss@naver.com>
2022-11-09 11:47:41 +0900

    22-11-09

--- bronze/string/_5622.py
+++ bronze/string/_5622.py
@@ -1,16 +1,38 @@
-dic_data = {
-    '2': ['A','B','C'],
-    '3': ['D','E','F'],
-    '4': ['G','H','I'],
-    '5': ['J','K','L'],
-    '6': ['M','N','O'],
-    '7': ['P','Q','R','S'],
-    '8': ['T','U','V'],
-    '9': ['W','X','Y','Z']
-}
+# dic_data = {
+#     '2': ['A','B','C'],
+#     '3': ['D','E','F'],
+#     '4': ['G','H','I'],
+#     '5': ['J','K','L'],
+#     '6': ['M','N','O'],
+#     '7': ['P','Q','R','S'],
+#     '8': ['T','U','V'],
+#     '9': ['W','X','Y','Z']
+# }
+#
+# dial = input()
+# num = 0
+# for i in dial:
+#     for key, values in dic_data.items():
+#         if i in values:
+#             num += int(key)
+#     num += 1
+#
+# print(num)
+#
+import sys
 
-dial = input()
+input = sys.stdin.readline()
 
-for i in dial:
-    for key, values in dic_data.items():
-        print(values)
+numbers = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+alpha = list(input)
+
+result = 0
+
+for i in alpha:
+    for j in numbers:
+        if i in j:
+            result += numbers.index(j) + 3
+print(result)
+
+
+
