3903c34b9676a3d04d2e9b5167b9d412a67f2355
sayskpays <76785132+sayskpays@users.noreply.github.com>
2022-12-05 19:37:21 +0900

    03 이진탐색

---
+++ programmers/data_structure/_03_이진탐색.py
@@ -0,0 +1,18 @@
+
+# lower = 0 # 리스트의 맨 처음 인덱스
+# upper = len(L) -1 # 리스트의 맨 끝 인덱스
+# idx = -1 # 원소가 발견되는 인덱스 위치
+
+
+# while lower <= upper:
+#     middle = (lower + upper) // 2
+#
+#     if L[middle] == target :
+#         pass
+#     elif L[middle] < target:
+#         lower = ...
+#     else:
+#         upper = ...
+
+
+
