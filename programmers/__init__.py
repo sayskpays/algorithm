ea71d11371bbb67920c0a42de667671f3cc9b6c6
hong <sayskpayss@naver.com>
2022-12-16 17:25:47 +0900

    lv1

--- programmers/__init__.py
+++ programmers/__init__.py
@@ -1,5 +1,29 @@
 
+s = input()
+new_arr = []
+answer = []
 
-arr = []
 
-print(arr[-1])
+for i in range(len(s)):
+    if s[i] not in new_arr:
+        answer.append(-1)
+    elif s[i] in new_arr:
+        if new_arr.count(s[i]) >= 2:
+            new_arr.pop(new_arr.index(s[i]))
+            idx_cnt = i - (new_arr.index(s[i])+1)
+        else:
+            idx_cnt = i - new_arr.index(s[i])
+        answer.append(idx_cnt)
+    new_arr.append(s[i])
+
+# for i in s:
+#     if i not in new_arr:
+#         new_arr.append(i)
+#         answer.append(-1)
+#     elif i in new_arr:
+#         idx_cnt = s.index(i) - new_arr.index(i)
+#         answer.append(idx_cnt)
+
+# 아직 해설 보지 않음 !!!!!!!!!!!
+
+print(answer)
