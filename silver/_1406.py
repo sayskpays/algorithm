7f200e4878b4b256c606525c44b570d5753e0e95
hong <sayskpayss@naver.com>
2023-01-26 10:47:40 +0900

    1406 silver 2 stack

--- silver/_1406.py
+++ silver/_1406.py
@@ -1,6 +1,6 @@
 # 내가 푼 코드 (시간 초과)
 
-# import sys
+import sys
 #
 # n = sys.stdin.readline().strip()
 # m = int(sys.stdin.readline().strip())
@@ -9,7 +9,7 @@
 #
 # for _ in range(m):
 #     ord = sys.stdin.readline().strip()
-#
+
 #     if ord == "L":
 #         if pointer == 0:
 #             pass
@@ -29,3 +29,29 @@
 #         stack.insert(pointer, ord.split(" ")[1])
 #
 # print("".join(stack))
+
+# 해설
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+
+    elif command[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
+
+    else:
+        st1.append(command[1])
+
+st1.extend(reversed(st2))
+print(''.join(st1))
--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# import sys
+import sys
 #
 # n = int(sys.stdin.readline().strip())
 # stack = []
@@ -24,32 +24,62 @@
 # if flag == 0:
 #     for i in answer:
 #         print(i)
-import sys
 
-n = sys.stdin.readline().strip()
-m = int(sys.stdin.readline().strip())
-stack = list(n)
-pointer = len(n) + 1
 
-for _ in range(m):
-    ord = sys.stdin.readline().strip()
+# n = sys.stdin.readline().strip()
+# m = int(sys.stdin.readline().strip())
+# stack = list(n)
+# pointer = len(n)
+#
+# for _ in range(m):
+#     ord = sys.stdin.readline().strip()
+#
+#     if ord == "L":
+#         if pointer == 0:
+#             pass
+#         else:
+#             pointer -= 1
+#     elif ord == "D":
+#         if pointer > len(n) + 1:
+#             pass
+#         else:
+#             pointer += 1
+#     elif ord == "B":
+#         if pointer <= 0:
+#             pass
+#         else:
+#             if pointer >= len(n):
+#                 stack.pop()
+#             else:
+#                 stack.pop(pointer-1)
+#     else:
+#         stack.insert(pointer, ord.split(" ")[1])
+#
+# print("".join(stack))
 
-    if ord == "L":
-        if pointer == 0:
-            pass
-        else:
-            pointer -= 1
-    elif ord == "D":
-        if pointer > len(n) + 1:
-            pass
-        else:
-            pointer += 1
-    elif ord == "B":
-        if pointer <= 0:
-            pass
-        else:
-            stack.pop(pointer-1)
+# 해설
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+
+    elif command[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
+
     else:
-        stack.insert(pointer, ord.split(" ")[1])
+        st1.append(command[1])
 
-print("".join(stack))
+st1.extend(reversed(st2))
+print(''.join(st1))
+
