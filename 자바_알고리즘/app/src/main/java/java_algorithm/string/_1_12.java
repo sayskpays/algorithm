e7646c30f85f728a4f096dfd6c7eb8d5111a0b47
sayskpays <76785132+sayskpays@users.noreply.github.com>
2025-10-08 16:33:51 +0900

    .

--- 자바_알고리즘/app/src/main/java/java_algorithm/string/_1_12.java
+++ 자바_알고리즘/app/src/main/java/java_algorithm/string/_1_12.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class _1_12 {
 
+    /* 내 풀이(정답)
     private static String solution(String in){
 
         String answer = "";
@@ -33,6 +34,18 @@ public class _1_12 {
 
         return answer;
     }
+    */
+    public static String solution(int n, String s){
+        String answer="";
+        for(int i=0; i<n; i++){
+            String tmp=s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num=Integer.parseInt(tmp, 2);
+            answer+=(char)num;
+            s=s.substring(7);
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
